b84b8c61aa3a2b04f7dc071103ac823c3162f45b
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2019-11-21 12:41:02 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/naver/hackday/devcenterbot/DevcenterBotApplication.java
@@ -0,0 +1,12 @@
+package com.naver.hackday.devcenterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcenterBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcenterBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/naver/hackday/devcenterbot/DevcenterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naver.hackday.devcenterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevcenterBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
