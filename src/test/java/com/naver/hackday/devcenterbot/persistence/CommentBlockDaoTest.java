5cbd1da9f59f08db761f4dd9f8b7ad3f82b5a2ab
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2019-11-21 19:54:21 +0900

    안 쓰이는 import 제거

--- src/test/java/com/naver/hackday/devcenterbot/persistence/CommentBlockDaoTest.java
+++ src/test/java/com/naver/hackday/devcenterbot/persistence/CommentBlockDaoTest.java
@@ -1,9 +1,7 @@
 package com.naver.hackday.devcenterbot.persistence;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
