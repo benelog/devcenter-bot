942c626f237ed6a1552252b88ea7780522332eb5
juhyeon96 <kjh96905@gmail.com>
2019-11-21 20:14:00 +0900

    fix botRequest

--- src/test/java/com/naver/hackday/devcenterbot/DevcenterBotApplicationTests.java
+++ src/test/java/com/naver/hackday/devcenterbot/DevcenterBotApplicationTests.java
@@ -1,13 +1,26 @@
 package com.naver.hackday.devcenterbot;
 
+import com.naver.hackday.devcenterbot.model.BotRequest;
+import com.naver.hackday.devcenterbot.model.RequestType;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DevcenterBotApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testBotRequest() {
+		BotRequest botRequest = new BotRequest();
+		String testing = "smart-editor";
+		BotRequest newRequest = botRequest.SMART_EDITOR_REQUEST.build();
+		assertThat(newRequest.getId()).isEqualTo(1);
+		assertThat(newRequest.getKeyword().getWord()).isEqualTo(testing);
 	}
 
 }
