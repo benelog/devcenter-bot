88614bd5858b4a9356d746e9e735f055999953d3
이창섭 <LeeChnagSup@ichangseobui-MacBook-Air.local>
2019-11-21 22:13:19 +0900

    refactoring code

--- src/main/java/com/naver/hackday/devcenterbot/model/TitleScrapper.java
+++ src/main/java/com/naver/hackday/devcenterbot/model/TitleScrapper.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 
 import org.eclipse.egit.github.core.Issue;
 import org.eclipse.egit.github.core.service.IssueService;
@@ -14,21 +15,21 @@ import org.eclipse.egit.github.core.service.IssueService;
 public class TitleScrapper {
 
 	public static void run() throws IOException {
+
 		//fileBase의 위치가 다를수도 있으므로..
-		String baseUrl = "/Users/LeeChnagSup/Desktop/코딩/NaverHackDay2019";
-		String fileName = baseUrl + "/devcenter-bot/src/main/resources/Files/log.txt";
+		String fileName =  "./src/main/resources/Files/log.txt";
 		FileReader input = new FileReader(new File(fileName));
 		BufferedReader br = new BufferedReader(input);
 		int checkNumber = Integer.parseInt(br.readLine());
 		IssueService issueService = new IssueService();
-//		System.out.println(I.pageIssueEvents("ventulus95", "BaekjoonAnswer",1));
 		HashMap<String, String> filter = new HashMap<String, String>();
 		filter.put("direction", "asc");
-		// 이슈는 무조건 숫자가 클수록 최신버전이 아님. 2. 0~10
-		int size = issueService.getIssues("ventulus95", "BaekjoonAnswer", filter).size();
+		// 이슈는 무조건 숫자가 클수록 최신버전이 아님. 2. 0~10..
+		List<Issue> listIssue = issueService.getIssues("ventulus95", "BaekjoonAnswer", filter);
+		int size = listIssue.size();
 		for (int num = checkNumber; num < size; num++) {
 			System.out.print("Issue #" + (num + 1) + " :");
-			Issue currentIssue = issueService.getIssues("ventulus95", "BaekjoonAnswer", filter).get(num);
+			Issue currentIssue = listIssue.get(num);
 			System.out.println(currentIssue.getTitle());
 			String issueTitle = currentIssue.getTitle();
 		}
