2e4dd73e806d79f5c78f77246cdd47643a4e0af1
Sanghyuk Jung <sanghyuk.jung@navercorp.com>
2019-11-22 11:28:07 +0900

    Message 클래스 리팩토링

--- src/test/java/com/naver/hackday/devcenterbot/MessageTest.java
+++ src/test/java/com/naver/hackday/devcenterbot/MessageTest.java
@@ -1,12 +1,27 @@
 package com.naver.hackday.devcenterbot;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.naver.hackday.devcenterbot.model.MessageModel;
 
 @SpringBootTest
 class MessageTest {
+	@Autowired
+	Message message;
 
 	@Test
-	void test1() {
+	void pushMessage() throws IOException {
+		MessageModel model = new MessageModel();
+		model.setName("kkyehit");
+		model.setRepoName("egit-github-test");
+		model.setIssueNum("2");
+		model.setComment("dev test test");
+		message.setModel(model);
+
+		message.pushMessage();
 	}
 }
